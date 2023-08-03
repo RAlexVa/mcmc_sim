57f5917503c596ed1c28cb3300b7a878a7bc2db1
noralex <ralexander.valencia@gmail.com>
2023-08-03 14:59:45 -0400

    Modified Q_unif and alpha_p functions
    Q_unif now can take both the number of adjacent states or a vector specifying the states.
    alpha_p was adapted to work with the same modification as Q_unif

--- RF_function.R
+++ RF_function.R
@@ -14,6 +14,41 @@
 # sample is the state corresponding to that multiplicity
 
 ##### AID FUNCTION #####
+# return uniform distribution for the specified neighbors
+#Q(i,j) ∝ 1
+Q_unif <- function(i,S, adj=0, nbr=NA){
+  #Returns neighbors and uniform distribution over those neighbors
+  if(!all(is.na(nbr))){
+    if(!all(nbr %in% S)){
+      print("some of the specified neighbors are not in S");return(NA)
+    }else{
+      neighbors <- nbr[nbr!=i]
+      n_size <- length(neighbors)
+      prob <- rep(1/n_size,n_size)
+      return(cbind(neighbors,prob))
+    }
+    
+  }else{
+    if(!(i %in% S)){print(paste("State",i,"is not in S"));return(NA)}else{#Check that state i is in S
+      if(adj < 0){print("adj parameter must be non-negative"); return(NA)}else{
+        if(adj == 0){ #Consider all neighbors
+          neighbors <- S[S!=i]
+        }else{ #Consider the number of adjacent neighbors defined by adj
+          index <- which(i==S) - 1 #index of the current state
+          adj_index <- c(-adj:-1,1:adj)
+          neighbors <- S[(adj_index + index)%%(length(S)) + 1]
+          neighbors <- unique(neighbors[neighbors != i]) #In case the # of adjacent neighbors is too big
+        }
+        #uniform proposal distribution
+        n_size <- length(neighbors)
+        prob <- rep(1/n_size,n_size)
+        return(cbind(neighbors,prob))
+      }
+    }
+  }
+}
+
+##### AID FUNCTION #####
 #Function to calculate the probability of escaping a state
 #INPUT
 # x: state to calculate escape probability
@@ -25,8 +60,8 @@
 # Matrix
 # 1st column is the neighbor
 # 2nd column is the ptobability of moving from state X to the state in that row
-alpha_p <- function(x,S,pi,Q, adj=0){
-  n_x <- Q(x,S,adj)
+alpha_p <- function(x,S,pi,Q, adj=0, nbr=NA){
+  n_x <- Q(x,S,adj,nbr)
   p_esc <- matrix(NA,ncol=2)
   for(y in n_x[,1]){
     n_y <- Q(y,S,adj)
@@ -82,25 +117,7 @@ mh_jump <- function(S,initial,B,M,pi,Q,adj=0){
 # set.seed(123)
 # S <- 1:5 #State space
 # pi <- exp(S/2) #Proportions of target distribution
-# #Q(i,j) ∝ 1
-# Q_unif <- function(i,S, adj=0){
-#   if(!(i %in% S)){print(paste("State",i,"is not in S"));return(NA)}else{#Check that state i is in S
-#     if(adj < 0){print("adj parameter must be non-negative"); return(NA)}else{
-#       if(adj == 0){ #Consider all neighbors
-#         neighbors <- S[S!=i]
-#       }else{ #Consider the number of adjacent neighbors defined by adj
-#         index <- which(i==S) - 1 #index of the current state
-#         adj_index <- c(-adj:-1,1:adj)
-#         neighbors <- S[(adj_index + index)%%(length(S)) + 1]
-#         neighbors <- unique(neighbors[neighbors != i]) #In case the # of adjacent neighbors is too big
-#       }
-#       #uniform proposal distribution
-#       n_size <- length(neighbors)
-#       prob <- rep(1/n_size,n_size)
-#       return(cbind(neighbors,prob))
-#     }
-#   }
-# }
+
 # ex <- mh_jump(S,1,50,10000,pi,Q)
 # est_prob <- ex |> #estimate the probability
 #   group_by(sample) |>
