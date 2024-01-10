12d854b6162dd966af134c892186865176508b9f
noralex <ralexander.valencia@gmail.com>
2024-01-10 12:06:42 -0500

    Modification to the Q  used in the calculation of alpha
    initially it used the original Q (without re-normalizing considering only the specified neighbors of the PNSet). Now it starts with the original Q then filters only the neighbors specified in the PNSet and then re-distributes the probability so it adds up to 1 and this is the new probability used in the calculation of the alpha.

--- PNS_unbiased_function.R
+++ PNS_unbiased_function.R
@@ -66,10 +66,16 @@ Q_unif <- function(i,S, adj=0, nbr=NA){
 # 1st column is the neighbor
 # 2nd column is the ptobability of moving from state X to the state in that row
 alpha_pns <- function(x,S,pi,Q,nbr=S){
+  p_esc <- matrix(NA,ncol=2) #To store probability of escaping
   n_x <- Q(x,S,adj=0) #Obtain transition probabilities considering all neighbors
-  p_esc <- matrix(NA,ncol=2)
+  n_x <- n_x[n_x[,1]%in%nbr,] #Select only the specified neighbors
+  n_x[,2] <- n_x[,2]/sum(n_x[,2]) #Re-distribute the probability considering only the specified neighbors
+  #Previous line is done to create the new Q for the PNSet
   for(y in intersect(n_x[,1],nbr)){
     n_y <- Q(y,S,adj=0)
+    n_y <- n_y[n_y[,1]%in%c(nbr,x),] #Select only the specified neighbors and the current state X
+    n_y[,2] <- n_y[,2]/sum(n_y[,2]) #Re-distribute the probability considering only the specified neighbors
+    #Previous line is done to create the new Q for the PNSet
     index_x <- which(n_y[,1]==x)
     index_y <- which(n_x[,1]==y)
     prob <- n_x[index_y,2]*min(1,(pi[y]*n_y[index_x,2])/(pi[x]*n_x[index_y,2]))
