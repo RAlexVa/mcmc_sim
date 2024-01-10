a60fb00049806026cb1fe86aac57bfb72ffe1eee
noralex <ralexander.valencia@gmail.com>
2024-01-10 12:38:25 -0500

    Created separate files for aid functions
    the 2 aid functions used in the code were transfered to independent files and here they're just called when needed

--- PNS_unbiased_function.R
+++ PNS_unbiased_function.R
@@ -15,75 +15,9 @@
 # mul is the multiplicity
 # sample is the state corresponding to that multiplicity
 
-##### AID FUNCTION #####
-# return uniform distribution for the specified neighbors
-#Q(i,j) ∝ 1
-#Option 1: Use parameter nbr to define specific neighbors to consider 
-#Option 2: Use adj to define the number of adjacent neighbors (to the current state i) to consider
-Q_unif <- function(i,S, adj=0, nbr=NA){
-  #Returns neighbors and uniform distribution over those neighbors
-  if(!all(is.na(nbr))){
-    if(!all(nbr %in% S)){
-      print("some of the specified neighbors are not in S");return(NA)
-    }else{
-      neighbors <- nbr[nbr!=i]
-      n_size <- length(neighbors)
-      prob <- rep(1/n_size,n_size)
-      return(cbind(neighbors,prob))
-    }
-    
-  }else{
-    if(!(i %in% S)){print(paste("State",i,"is not in S"));return(NA)}else{#Check that state i is in S
-      if(adj < 0){print("adj parameter must be non-negative"); return(NA)}else{
-        if(adj == 0){ #Consider all neighbors
-          neighbors <- S[S!=i]
-        }else{ #Consider the number of adjacent neighbors defined by adj
-          index <- which(i==S) - 1 #index of the current state
-          adj_index <- c(-adj:-1,1:adj)
-          neighbors <- S[(adj_index + index)%%(length(S)) + 1]
-          neighbors <- unique(neighbors[neighbors != i]) #using unique in case the # of adjacent neighbors is too big
-        }
-        #uniform proposal distribution
-        n_size <- length(neighbors)
-        prob <- rep(1/n_size,n_size)
-        return(cbind(neighbors,prob))
-      }
-    }
-  }
-}
-
-
-##### AID FUNCTION #####
-#Function to calculate the probability of escaping a state in the PNS setting
-#INPUT
-# x: state to calculate escape probability
-# S: state space
-# pi: target distribution
-# Q: (function) proposal distribution
-# nbr: Neighbors to consider
-#OUTPUT
-# Matrix
-# 1st column is the neighbor
-# 2nd column is the ptobability of moving from state X to the state in that row
-alpha_pns <- function(x,S,pi,Q,nbr=S){
-  p_esc <- matrix(NA,ncol=2) #To store probability of escaping
-  n_x <- Q(x,S,adj=0) #Obtain transition probabilities considering all neighbors
-  n_x <- n_x[n_x[,1]%in%nbr,] #Select only the specified neighbors
-  n_x[,2] <- n_x[,2]/sum(n_x[,2]) #Re-distribute the probability considering only the specified neighbors
-  #Previous line is done to create the new Q for the PNSet
-  for(y in intersect(n_x[,1],nbr)){
-    n_y <- Q(y,S,adj=0)
-    n_y <- n_y[n_y[,1]%in%c(nbr,x),] #Select only the specified neighbors and the current state X
-    n_y[,2] <- n_y[,2]/sum(n_y[,2]) #Re-distribute the probability considering only the specified neighbors
-    #Previous line is done to create the new Q for the PNSet
-    index_x <- which(n_y[,1]==x)
-    index_y <- which(n_x[,1]==y)
-    prob <- n_x[index_y,2]*min(1,(pi[y]*n_y[index_x,2])/(pi[x]*n_x[index_y,2]))
-    p_esc <- rbind(p_esc,c(y,prob))
-  }
-  p_esc <- p_esc[-1,]
-  return(p_esc) 
-}
+##### AID FUNCTIONS #####
+source('functions/Q_unif.R')
+source('functions/alpha_pns.R')
 
 ##### FUNCTION #####
 PNS_unbiased <- function(S,initial,M,L,pi,Q,nbr_func){
